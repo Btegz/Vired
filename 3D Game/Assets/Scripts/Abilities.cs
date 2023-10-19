6c981d53dd3621c8646cc9f7f2d9ba03a0322924
Lisa <lisa@zehndercnc.de>
2023-10-19 16:31:24 +0200

    PlayerManager 19.10.2023
    Script: Abilities, PlayerManager

---
+++ 3D Game/Assets/Scripts/Abilities.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abilities : MonoBehaviour
+{
+    // Name 
+    // Effekt 
+    // Form 
+    // Kosten 
+    // Ressource (Schere/Stein/Papier System)
+}
