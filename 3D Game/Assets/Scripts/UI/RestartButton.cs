74f338ff7ab11b74164424d0a012e9362b8cd8d4
Kirifox42 <134942102+Kirifox42@users.noreply.github.com>
2023-10-30 20:28:36 +0100

    LoseScene: Add: Restart Button and Text + Tweening

---
+++ 3D Game/Assets/Scripts/UI/RestartButton.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
+
+public class RestartButton : MonoBehaviour
+{
+
+    public Button restartButton;
+    [SerializeField] private TextMeshProUGUI restartTMP;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        restartButton.onClick.AddListener(Restart);
+        //restartTMP = GetComponent<TextMeshProUGUI>();
+    }
+
+    private void Start()
+    {
+        restartTMP.DOFade(0f, 0f).SetDelay(0.5f);
+        restartTMP.DOFade(1f, 1f).SetDelay(1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("PrebuiltScene");
+    }
+}
