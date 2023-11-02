7d73406a03d26164c06465713b3d754fe30595ce
Kirifox42 <134942102+Kirifox42@users.noreply.github.com>
2023-11-02 21:05:09 +0100

    Add: CelineScene: MovePoints: Sprites and Tweening

---
+++ 3D Game/Assets/Scripts/UI/MovePointsDoTween.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class MovePointsDoTween : MonoBehaviour
+{
+
+    public RectTransform rectTransform;
+    public Sprite usedMovepoints;
+    public Sprite Movepoints;
+ 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Away ()
+    {
+        //rectTransform.transform.localPosition = new Vector3(-100f, 0f, 0f);
+        //rectTransform.DOAnchorPos(new Vector2(1000f, 10f), 1f, false).SetEase(Ease.OutElastic);
+        //rectTransform.DOPunchAnchorPos(new Vector2(20f, 0f), 1f).SetEase(Ease.OutElastic);
+        rectTransform.DOPunchPosition(new Vector2(20f, 0f), 1f).SetEase(Ease.OutElastic);
+
+        Image image = GetComponent<Image>();
+        image.sprite = usedMovepoints;
+        //image.sprite = Movepoints;
+        //usedMovepoints.SetActive(true);
+
+    }
+}
