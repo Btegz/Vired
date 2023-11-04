c452233b304e5765eeb69bf89e29d89cde1d3885
Btegz <137159255+Btegz@users.noreply.github.com>
2023-11-04 18:13:23 +0100

    Camera:Add:Movement/ Zoom
    To do:
    SmoothDamp
    Top Down

--- 3D Game/Assets/Scripts/CameraRotation.cs
+++ 3D Game/Assets/Scripts/CameraRotation.cs
@@ -7,25 +7,29 @@ using UnityEngine.InputSystem;
 
 public class CameraRotation : MonoBehaviour
 {
-    [SerializeField] private InputActionReference rotatoAction;
+    [SerializeField] InputActionReference rotatoAction;
     [SerializeField] InputActionReference zoomAction;
-
-    [SerializeField] float speed = 500;
+    [SerializeField] InputActionReference movementAction;
     [SerializeField] Camera cam;
-    
-    private Coroutine rotationCoroutine;
-    private Coroutine zoomCoroutine;
 
+    [SerializeField] float rotationSpeed = 500;
+    [SerializeField] float movementSpeed = 50;
+    [SerializeField] private float speedAdaption = 0.2f;
+    
+    [SerializeField] Vector3 endPosition;
+
+    private float mouseScrollY;
+     
+    private Coroutine rotationCoroutine;
+    private Coroutine movementCoroutine;
+    
     private Vector3 camPosition;
     private Vector3 startingPosition;
     
 
-    public float mouseScrollY;
-    // Start is called before the first frame update
-    void Start()
-    {
-        startingPosition = gameObject.transform.GetChild(0).transform.localPosition;
-    }
+   
+  
+    
     void Awake()
     {
         rotatoAction.action.Enable();
@@ -34,8 +38,17 @@ public class CameraRotation : MonoBehaviour
 
         zoomAction.action.Enable();
         zoomAction.action.performed += x => mouseScrollY = x.ReadValue<float>();
+        
+        movementAction.action.Enable();
+        movementAction.action.started += StartMovement;
+        movementAction.action.canceled += StopMovement ;
     }
-
+    
+    void Start()
+    {
+        startingPosition = gameObject.transform.GetChild(0).transform.localPosition;
+    }
+    
     void StopRotato(InputAction.CallbackContext obj)
     {
         StopCoroutine(rotationCoroutine);
@@ -44,28 +57,43 @@ public class CameraRotation : MonoBehaviour
     {
         rotationCoroutine = StartCoroutine(Rotato());
     }
-
-
-
+    
+    void StartMovement(InputAction.CallbackContext obj)
+    {
+        movementCoroutine = StartCoroutine(Movement());
+    } 
+    void StopMovement(InputAction.CallbackContext obj)
+    {
+        StopCoroutine(movementCoroutine);
+    }
+    
+    
     private void Update()
     {
-    
+        endPosition = new Vector3(0, 1, 2);
         camPosition = gameObject.transform.GetChild(0).transform.localPosition;
-
+        
+        /// Zoom +
+        /// Lerp von der Kamera Position durch die End Positon mit dem Scroll Input 
         if (mouseScrollY > 0 )
         {  
-            gameObject.transform.GetChild(0).transform.localPosition =
-                Vector3.Lerp(camPosition,Vector3.zero, mouseScrollY * Time.deltaTime );
+            gameObject.transform.GetChild(0).transform.localPosition = Vector3.Lerp(camPosition, endPosition, mouseScrollY * Time.deltaTime );
+            movementSpeed = (movementSpeed * (1 - speedAdaption))+1;        
         }
-        
+      
+        /// Zoom -
+        /// Lerp von der anfangs Kamera Position durch die aktuelle KameraPosition mit dem Scroll Input 
         if (mouseScrollY < 0)
         {
-            Debug.Log(startingPosition);
-            Debug.Log((mouseScrollY * Time.deltaTime));
-            gameObject.transform.GetChild(0).transform.localPosition =
-                Vector3.Lerp(startingPosition,camPosition, ((mouseScrollY * Time.deltaTime )+1));
+            gameObject.transform.GetChild(0).transform.localPosition = Vector3.Lerp(startingPosition,camPosition, ((mouseScrollY * Time.deltaTime )+1));
+            movementSpeed = (movementSpeed * (1 + speedAdaption))+1;
         }
     }
+    
+    /// <summary>
+    /// Rotation der Kamera um das Spielfeld mit der rechten Maustaste
+    /// </summary>
+    /// <returns></returns>
     IEnumerator Rotato()
     {
         Vector2 previousMousePosition = Pointer.current.position.ReadValue();
@@ -73,16 +101,25 @@ public class CameraRotation : MonoBehaviour
         while (true)
         {
             Vector2 currentMousePosition = Pointer.current.position.ReadValue();
-            transform.Rotate(Vector3.up, (currentMousePosition - previousMousePosition).x / Screen.width * speed);
-
+            transform.Rotate(Vector3.up, ((currentMousePosition - previousMousePosition).x / Screen.width) * rotationSpeed);
             previousMousePosition = currentMousePosition;
             yield return null;
         }
-        //Movement x und z Position des Parents 
     }
 
+    /// <summary>
+    /// Movement der Kamera auf der x und z Achse mit der mittleren Maustaste
+    /// </summary>
+    /// <returns></returns>
     IEnumerator Movement()
     {
-        yield return null;
+        Vector3 previousMousePosition = Pointer.current.position.ReadValue();
+        while (true)
+        {
+            Vector3 currentMousePosition = Pointer.current.position.ReadValue();
+            gameObject.transform.GetChild(0).transform.localPosition += new Vector3((-(currentMousePosition - previousMousePosition).x / Screen.width) * movementSpeed, transform.localPosition.y,((-(currentMousePosition - previousMousePosition).y / Screen.width) * movementSpeed));
+            previousMousePosition = currentMousePosition;
+            yield return null;
+        }
     }
 }
