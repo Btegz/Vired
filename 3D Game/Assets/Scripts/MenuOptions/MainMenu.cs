d55df05769665146a0cfd7734909c95261ca8092
Kirifox42 <134942102+Kirifox42@users.noreply.github.com>
2023-12-06 20:04:32 +0100

    Add: MainMenuScene: WiP

---
+++ 3D Game/Assets/Scripts/MenuOptions/MainMenu.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject optionCanvas;
+
+    public Button gameStartButton;
+    public Button optionsButton;
+    public Button exitButton;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        gameStartButton.onClick.AddListener(StartGame);
+        optionsButton.onClick.AddListener(OptionsSwitch);
+        exitButton.onClick.AddListener(ExitGame);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("CelineMeanTweenMemeSheenScene");
+    }
+
+    public void OptionsSwitch()
+    {
+        gameObject.SetActive(false);
+        optionCanvas.SetActive(true);
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
