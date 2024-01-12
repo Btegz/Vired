a32373adc136a7d35c7a0723d857181b97e53b54
Lisa Zehnder <lisa@zehndercnc.de>
2024-01-12 20:05:19 +0100

    Shader: Implement: Glitch
    - Glitch finished
    - Glitch implemented
    
    To do:
    - Glitch Texture Background

--- 3D Game/Assets/Scripts/UI/ButtonScale.cs
+++ 3D Game/Assets/Scripts/UI/ButtonScale.cs
@@ -6,15 +6,24 @@ using DG.Tweening;
 using UnityEngine.EventSystems;
 
 public class ButtonScale : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
-{ 
+{
+
+    public Material glitchMaterial;
+    public Image turnNextButton;
+    
     public void OnPointerEnter(PointerEventData eventData)
     {
         transform.DOScale(1.08f, 0.15f);
+
+        turnNextButton.material = glitchMaterial;
+
+
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         transform.DOScale(1f, 0.1f);
+        turnNextButton.material = null;
     }
 }
 
