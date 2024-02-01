ae765ba17d8dc8e126e4e7c99b3d78e0f844b9cf
Lisa Zehnder <lisa@zehndercnc.de>
2024-02-01 17:55:37 +0100

    Move

--- 3D Game/Assets/Scripts/Player/PlayerManager.cs
+++ 3D Game/Assets/Scripts/Player/PlayerManager.cs
@@ -329,7 +329,7 @@ public Player selectedPlayer;
         
         SaveManager.Instance.totalMovement++;
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(0.35f);
         hovern.volume = PlayerPrefs.GetFloat("HoverVolume");
         move = true;
         yield return null;
